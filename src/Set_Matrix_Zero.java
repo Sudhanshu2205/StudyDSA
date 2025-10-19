24040d24157df66213b2fb171425818947a5aa1b
Sudhanshu2205 <sudhanshu060722@gmail.com>
2025-10-19 18:17:30 +0530

    1st Dsa Question

---
+++ src/Set_Matrix_Zero.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    static void markrow(ArrayList<ArrayList<Integer>> matrix, int n, int m, int i) {
+        for (int j = 0; j < m; j++) {
+            if (matrix.get(i).get(j) != 0) {
+                matrix.get(i).set(j, -1);
+            }
+        }
+    }
+
+    static void markcol(ArrayList<ArrayList<Integer>> matrix, int n, int m, int j) {
+        for (int i = 0; i < n; i++) {
+            if (matrix.get(i).get(j) != 0) {
+                matrix.get(i).set(j, -1);
+            }
+        }
+    }
+
+    static ArrayList<ArrayList<Integer>> setzero(ArrayList<ArrayList<Integer>> matrix, int m, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    markrow(matrix, m, n, i);
+                    markcol(matrix, m, n, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == -1) {
+                    matrix.get(i).set(j, 0);
+                }
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+
+        int n = matrix.size();
+        int m = matrix.get(0).size();
+
+        ArrayList<ArrayList<Integer>> ans = setzero(matrix, m, n);
+        System.out.println("The Final Matrix is : ");
+        for (ArrayList<Integer> row : ans) {
+            for (Integer ele : row) {
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+    }
+}
